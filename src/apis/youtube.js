7e568f09deca6cb6526c28efc6c52fbc3b672aa4
Maanum <kristofer.maanum@gmail.com>
2020-05-27 09:05:56 -0500

    Completed project

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// Youtube Data APIv3 API Key
+const KEY = process.env.KEY;
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
